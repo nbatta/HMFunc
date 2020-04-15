1b807907077d2ac17f1f5adb07ee281fc6599a9e
Nicholas Battaglia <nbatta@astro.cornell.edu>
2020-04-15 10:01:07 -0400

    install function and simple notebook added

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup, Extension
+import os
+
+setup(name='HMFunc',
+      version='0.1',
+      description='Simple Halo mass function tool with a comparison wrapper to Colossus',
+      url='https://github.com/nbatta/HMFunc',
+      author='Nicholas Battaglia',
+      author_email='nicholas.battaglia@gmail.com',
+      license='BSD-2-Clause',
+      packages=['HMFunc'],
+      package_dir={'HMFunc':'src'},
+      zip_safe=False)
